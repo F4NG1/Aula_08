3c08a674e07db1a58b25462a57871409c010d839
PEDRO ALVES DOS SANTOS <asantos.pedro@aluno.senac.rio>
2025-12-09 21:55:20 -0300

    Função

---
+++ Exemplo3.py
@@ -0,0 +1,17 @@
+# def saudacao(n,i):
+#     print(f'Olá, {n} .Como vai?  Sua idade é {i}')
+
+
+# nome=input('Informe o nome: ')
+# idade=input('Informe a idade: ')
+# saudacao(nome,idade)
+
+
+def soma(n1,n2):
+    soma1=n1+n2
+    print(f'O resulta é: {soma1} ')
+   
+   
+numero1=float(input('Insira o primeiro número: '))
+numero2=float(input('Insira o segundo número: '))
+soma(numero1,numero2)
